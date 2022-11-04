c0d92b2d218acf09c2c7daae485592fefcbb8f8a
Hung <manhhungnguyen284@gmail.com>
2022-11-04 10:44:42 +0700

    test commit

--- app/src/main/java/com/example/duan1/MainActivity.java
+++ app/src/main/java/com/example/duan1/MainActivity.java
@@ -2,18 +2,21 @@ package com.example.duan1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.media.Image;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-TextView tvName;
+TextView tvName, tvTest;
 ImageView img;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvTest = findViewById(R.id.tv_test);
     }
 }
