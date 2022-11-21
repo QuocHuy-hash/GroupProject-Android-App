76cb277afdb954a75f742c85523dfd1b7ccf071d
QuocHuy <huy343536@gmial.com>
2022-11-21 15:07:42 +0700

    add layout choose listProduct

--- app/src/main/java/com/example/duan1/MainActivity.java
+++ app/src/main/java/com/example/duan1/MainActivity.java
@@ -4,10 +4,17 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.example.duan1.Fragment.FragmentDaoCho;
 import com.example.duan1.Fragment.FragmentQuanLiTin;
@@ -38,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, DangTin.class));
+                showDialogChooseListProduct(Gravity.CENTER);
             }
         });
 
@@ -71,6 +78,80 @@ public class MainActivity extends AppCompatActivity {
         });
         
     }
+
+    private void showDialogChooseListProduct(int gravity) {
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.layout_dialog_chon_danh_muc);
+
+        Window window = dialog.getWindow();
+        if(window == null) {
+            return;
+        }
+        window.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);
+
+        WindowManager.LayoutParams windowAttributes = window.getAttributes();
+        windowAttributes.gravity = gravity;
+        window.setAttributes(windowAttributes);
+
+        RelativeLayout layout_batDongSan = dialog.findViewById(R.id.layout_batDongSan);
+        RelativeLayout layout_XeCo = dialog.findViewById(R.id.layout_XeCo);
+        RelativeLayout layout_thoiTrang = dialog.findViewById(R.id.layout_thoiTrang);
+        RelativeLayout layout_dienTu = dialog.findViewById(R.id.layout_dienTu);
+        RelativeLayout layout_giaiTri = dialog.findViewById(R.id.layout_giaiTri);
+        RelativeLayout layout_dienGiaDung = dialog.findViewById(R.id.layout_dienGiaDung);
+        ImageView icon_close = dialog.findViewById(R.id.icon_close);
+
+        icon_close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+        layout_giaiTri.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext() , chonDanhMucGiaiTriActivity.class));
+            }
+        });
+        layout_dienGiaDung.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext() , chonDanhMucDienMayActivity.class));
+
+            }
+        });
+        layout_batDongSan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "danh mục batDongSan", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(getApplicationContext() , chonDanhMucBDSActivity.class));
+            }
+        });
+        layout_XeCo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext() , chonDanhMucXeCoActivity.class));
+
+            }
+        });
+        layout_thoiTrang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext() , chonDanhMucThoiTrangAcrivity.class));
+
+            }
+        });
+        layout_dienTu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext() , chonDanhMucDoDienTuActivity.class));
+
+            }
+        });
+        dialog.show();
+    }
+
 
     private void initUi() {
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
