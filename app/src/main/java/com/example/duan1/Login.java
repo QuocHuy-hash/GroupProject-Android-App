060705f4bfc9bedd16e1d2a7da629be21115b622
Hung <manhhungnguyen284@gmail.com>
2022-11-23 23:35:25 +0700

    Login FB, Reload Fragment

--- app/src/main/java/com/example/duan1/Login.java
+++ app/src/main/java/com/example/duan1/Login.java
@@ -1,5 +1,6 @@
 package com.example.duan1;
 
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -7,6 +8,7 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,11 +16,24 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
 import com.google.firebase.auth.FirebaseUser;
+
+
+import java.util.Arrays;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -32,6 +47,7 @@ public class Login extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private ProgressDialog progressDialog;
+    CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +55,33 @@ public class Login extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         initUi();
 
+        callbackManager = CallbackManager.Factory.create();
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+//                        handleFacebookAccessToken(loginResult.getAccessToken());
+                        Toast.makeText(Login.this, "Đăng nhập thành công!", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(Login.this, MainActivity.class);
+//                                FirebaseUser currentUser = mAuth.getCurrentUser();
+//                                String name = currentUser.getDisplayName();
+//                                String image = String.valueOf(currentUser.getPhotoUrl());
+//                                intent.putExtra("name",name );
+//                                intent.putExtra("image", image);
+                        setResult(RESULT_OK,intent );
+                        finish();
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        // App code
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        // App code
+                    }
+                });
 
         initListenerClick();
 
@@ -77,7 +120,7 @@ public class Login extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 //code login facebook
-                Toast.makeText(Login.this, "Login fb from Login", Toast.LENGTH_SHORT).show();
+                LoginManager.getInstance().logInWithReadPermissions(Login.this, Arrays.asList("public_profile"));
             }
         });
 
@@ -150,7 +193,9 @@ public class Login extends AppCompatActivity {
                                         if (task.isSuccessful()) {
                                             // Sign in success, update UI with the signed-in user's information
                                             Toast.makeText(Login.this, "Đăng nhập thành công!", Toast.LENGTH_SHORT).show();
-
+                                            Intent intent = new Intent(Login.this, MainActivity.class);
+                                            intent.putExtra("name", email);
+                                            setResult(RESULT_OK,intent );
                                             finish();
                                         } else {
                                             // If sign in fails, display a message to the user.
@@ -166,5 +211,29 @@ public class Login extends AppCompatActivity {
 
     }
 
+    private void handleFacebookAccessToken(AccessToken token) {
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(Login.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // Pass the activity result back to the Facebook SDK
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
 
 }
