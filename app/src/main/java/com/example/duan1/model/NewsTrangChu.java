9a616060266b0b1be56f89fb41e9b6027a214644
daoh329 <daoh329@gmail.com>
2022-12-05 20:03:11 +0700

    commitcodeonmybranches

---
+++ app/src/main/java/com/example/duan1/model/NewsTrangChu.java
@@ -0,0 +1,70 @@
+package com.example.duan1.model;
+
+public class NewsTrangChu {
+    private String title,descripsion, fee, time;
+    private boolean favorite;
+    private String ArrURL;
+//    public int getsoluonganh(){
+//        return getArrURL().size();
+//    }
+
+    public NewsTrangChu() {
+    }
+
+    public NewsTrangChu(String title, String descripsion, String fee, String time, boolean favorite, String arrURL) {
+        this.title = title;
+        this.descripsion = descripsion;
+        this.fee = fee;
+        this.time = time;
+        this.favorite = favorite;
+        ArrURL = arrURL;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescripsion() {
+        return descripsion;
+    }
+
+    public void setDescripsion(String descripsion) {
+        this.descripsion = descripsion;
+    }
+
+    public String getFee() {
+        return fee;
+    }
+
+    public void setFee(String fee) {
+        this.fee = fee;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public boolean isFavorite() {
+        return favorite;
+    }
+
+    public void setFavorite(boolean favorite) {
+        this.favorite = favorite;
+    }
+
+    public String getArrURL() {
+        return ArrURL;
+    }
+
+    public void setArrURL(String arrURL) {
+        ArrURL = arrURL;
+    }
+}
